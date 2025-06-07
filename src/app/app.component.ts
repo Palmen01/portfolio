33ba3087a0f862051e9c69e2b625bac2e9a5817a
Palmen01 <144669500+Palmen01@users.noreply.github.com>
2025-06-07 21:32:59 +0200

    about me component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { Router } from "@angular/router";
 import { HeroComponent } from './hero/hero.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { SkilltreeComponent } from './skilltree/skilltree.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, CarouselComponent, SkilltreeComponent],
+  imports: [NavbarComponent, HeroComponent, CarouselComponent, SkilltreeComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
