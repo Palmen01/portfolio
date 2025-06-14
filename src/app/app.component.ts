3cb0554227138be071d15ecadb5a139098b3a337
Palmen01 <144669500+Palmen01@users.noreply.github.com>
2025-06-14 20:42:13 +0200

    footer progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { SkilltreeComponent } from './skilltree/skilltree.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, CarouselComponent, SkilltreeComponent, AboutMeComponent, ExperienceComponent],
+  imports: [NavbarComponent, HeroComponent, CarouselComponent, SkilltreeComponent, AboutMeComponent, ExperienceComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
